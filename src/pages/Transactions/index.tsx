bc79e67310650c5f47e8ac05e2ac31f9ed5c3a9f
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-05-08 23:56:31 -0300

    iniciando projeto

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,43 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SeachForm";
+import { PriceHightLight, TransactionsContainer, TransactionsTable } from "./styles";
+
+export function Transactions() {
+    return (
+        <div>
+            <Header />
+            <Summary />
+
+            <TransactionsContainer>
+                <SearchForm />
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHightLight variant="income">
+                                    R$ 12.000,00
+                                </PriceHightLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2025</td>
+                        </tr>
+
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <PriceHightLight variant="outcome">
+                                    - R$ 6000,00
+                                </PriceHightLight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>10/04/2025</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
+
+        </div>
+    )
+}
