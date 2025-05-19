7c2968f353657961679be6acb3a0b52607e10c82
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-05-18 21:18:43 -0300

    adicionando React Hook Form nos formularios

--- src/pages/Transactions/components/SeachForm/index.tsx
+++ src/pages/Transactions/components/SeachForm/index.tsx
@@ -1,12 +1,41 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod';
 
-export function SearchForm(){
-    return(
-        <SearchFormContainer>
-            <input type="text" placeholder="Busque a transação" />
-            <button>
-                <MagnifyingGlass size={20}/>
+const searchFormSchema = z.object({
+    query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
+export function SearchForm() {
+
+
+    const {
+        register,
+        handleSubmit,
+        formState: { isSubmitting } //isSubmitting retorna um true ou false dizendo se o formulario esta em estado de enviar informação ou não
+    } = useForm<SearchFormInputs>({
+        //Handle é uma forma padrão para identificarmos que o dado veio de uma ação do usuario
+        resolver: zodResolver(searchFormSchema)
+    })
+
+    async function handleSearchTransactions(data: SearchFormInputs) {
+        await new Promise(resolve => setTimeout(resolve,2000)) //bloqueamos o botão em tempo de resposta da api
+        console.log(data)
+    }
+
+    return (
+        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+            <input
+                type="text"
+                placeholder="Busque a transação"
+                {...register('query')}
+            />
+            <button type="submit" disabled={isSubmitting}>
+                <MagnifyingGlass size={20} />
                 Buscar
             </button>
         </SearchFormContainer>
